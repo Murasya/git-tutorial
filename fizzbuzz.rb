0672a5b17b5af2c0f7bb2a750c480f6cd9dd2d9c
Murakoso Kosei <j1436@matsue-ct.jp>
2017-11-21 09:56:20 +0900

    pをprintに変更

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 1.upto(100){|num|
 	if num % 3 == 0 && num % 5 == 0
-		p "FizzBuzz"
+		print "FizzBuzz\n"
 	elsif num % 3 == 0
-		p "Fizz"
+		print "Fizz\n"
 	elsif num % 5 == 0
-		p "Buzz"
+		print "Buzz\n"
 	else
 		p num
 	end
